21539d850bbab6289726e503e865c1266e58f207
Kingswill Wariboko <kingswillwariboko@gmail.com>
2022-12-12 16:57:25 +0100

    another change

--- src/component/shop/Shop.js
+++ src/component/shop/Shop.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {Suspense, useRef, useState} from 'react'
+import { Canvas} from '@react-three/fiber'
+import {OrbitControls, useGLTF} from '@react-three/drei'
 import vector from "../../assets/Vector.svg"
 import love from "../../assets/heart.svg"
-import chair from "../../assets/pngwing 1.svg"
 import arbutton from "../../assets/icon-park-outline_virtual-reality-glasses.svg"
 import cart from "../../assets/shopping-cart.svg"
-
-
 import "./shop.scss"
+
+
+
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  return (
+    <group ref={group} {...props} dispose={null} scale={3}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh}/>
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch}/>
+    </group>
+  )
+}
 
 const Shop = () => {
   return (
@@ -20,7 +38,20 @@ const Shop = () => {
         </header>
 
         <div className='shop__product'>
-            <img src={chair} alt="" />
+        <Canvas>
+                      <Suspense fallback={null}>
+                          <ambientLight />
+                          <spotLight intensity={0.9} 
+                                     angle={0.1} 
+                                     penumbra={1} 
+                                     position={[10,15,10]}
+                                     castShadow />
+                          <Model />
+                          <OrbitControls enablePan={true}
+                                         enableZoom={true}
+                                         enableRotate={true}/>
+                      </Suspense>
+                   </Canvas>
         </div>
 
         <div className='shop__info'>
