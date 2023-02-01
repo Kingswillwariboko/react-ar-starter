9a0c47ee1e7cd564574061ec4ce8de6bbef430c4
Kingswill Wariboko <kingswillwariboko@gmail.com>
2023-02-01 12:54:46 +0100

    another change

--- src/component/shop/Shop.js
+++ src/component/shop/Shop.js
@@ -2,7 +2,7 @@ import React from 'react'
 import vector from "../../assets/Vector.svg"
 import love from "../../assets/heart.svg"
 import cart from "../../assets/shopping-cart.svg"
-import chair from "../../assets/Kobiri Go Express .glb"
+import chair from "../../assets/chair.glb"
 import ar from "../../assets/augmented-reality.png"
 import "./shop.scss"
 
@@ -34,7 +34,6 @@ const Shop = () => {
                 </div>
 
             </div>
- 
             <div className='shop__info-description'>
                 <p>The sleek and stylish Casablanca sofa is the perfect addition to
                      any modern living room. Made with high-quality materials that offers
@@ -42,7 +41,6 @@ const Shop = () => {
                     the Casablanca sofa is sure to elevate your living space.</p>
             </div>
         </div>
-
        <div className='shop__cartt'>
             <div className='shop__cartt-img'>
                 <img src={cart} alt="" />
